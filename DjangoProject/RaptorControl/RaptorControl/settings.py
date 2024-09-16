e9d139add66dff23802f04bc72342b362636a9fd
key <key@dhcp-10-145-219-237.taximaxim.local>
2024-09-16 09:28:21 +0500

    test commit

--- DjangoProject/RaptorControl/RaptorControl/settings.py
+++ DjangoProject/RaptorControl/RaptorControl/settings.py
@@ -121,6 +121,6 @@ STATIC_URL = 'static/'
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-fiel
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-fie
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
