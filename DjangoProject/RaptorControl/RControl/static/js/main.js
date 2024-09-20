40f4fbe68eca63508e787ed4d1df251648396623
key <key@dhcp-10-145-219-237.taximaxim.local>
2024-09-20 11:53:57 +0500

    Added hidden and reveal sidebar

--- DjangoProject/RaptorControl/RControl/static/js/main.js
+++ DjangoProject/RaptorControl/RControl/static/js/main.js
@@ -2,11 +2,14 @@
 const devices = [
     { id: 1, uuid: '123e4567-e89b-12d3-a456-426614174000', user: 'user1', name: 'device1', ip: '192.168.1.1', platform: 'Linux', os: 'Ubuntu 20.04', lastRequest: '2023-10-01 12:00:00', status: 'active' },
     { id: 2, uuid: '123e4567-e89b-12d3-a456-426614174001', user: 'user2', name: 'device2', ip: '192.168.1.2', platform: 'Windows', os: 'Windows 10', lastRequest: '2023-10-01 12:05:00', status: 'inactive' },
+    { id: 3, uuid: '123e4567-e89b-12d3-a456-426614174002', user: 'user3', name: 'device3', ip: '192.168.1.3', platform: 'Linux', os: 'Debian 10', lastRequest: '2023-10-01 12:10:00', status: 'active' },
+    { id: 4, uuid: '123e4567-e89b-12d3-a456-426614174003', user: 'user4', name: 'device4', ip: '192.168.1.4', platform: 'Linux', os: 'Ubuntu 22.04', lastRequest: '2023-10-01 12:15:00', status: 'inactive' },
     // Добавьте дополнительные устройства по мере необходимости
 ];
 
 function loadDevices() {
     const tbody = document.getElementById('device-table-body');
+    tbody.innerHTML = ''; // Очищаем таблицу перед загрузкой
     devices.forEach(device => {
         const row = document.createElement('tr');
         row.innerHTML = `
@@ -23,10 +26,16 @@ function loadDevices() {
     });
 }
 
-function toggleDevices(category) {
-    // Логика для переключения видимости устройств по категориям
-    console.log(`Таб ${category} был нажат`);
+
+function toggleDevicesList() {
+    const devicesList = document.getElementById('devices-list');
+    if (devicesList.style.maxHeight) {
+        devicesList.style.maxHeight = null; // Закрываем
+    } else {
+        devicesList.style.maxHeight = devicesList.scrollHeight + "px"; // Открываем
+    }
 }
+
 
 // Загрузка устройств при загрузке страницы
 window.onload = loadDevices;
