2d8224e05fc5b36b17b57137195f551146efba95
KeyJackei <shchukin.slava4@gmail.com>
2025-05-05 09:12:01 +0500

    Changed name of module

--- RaptorControl/urls.py
+++ RaptorControl/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
+    path('', include('Users.urls')),
     path('main/', include('RControl.urls')),
     path('requests/', include('Requests.urls'))
 
